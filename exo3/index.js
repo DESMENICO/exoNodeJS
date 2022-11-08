01f1161a06599ccd1bb3d8fa724b18051485aab6
DESMENICO <81149198+DESMENICO@users.noreply.github.com>
2022-11-08 15:02:18 +0100

    exo3

---
+++ exo3/index.js
@@ -0,0 +1,24 @@
+const path = require ('path');
+const express = require('express')
+const app = express()
+const port = 3000
+
+const page1 = path.join("page.html")
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"public","html","page.html"));
+})
+
+app.get('/page2', (req, res) => {
+    res.sendFile(path.join(__dirname,"public","html","page2.html"));
+})
+
+app.get('/Hello', (req, res) => {
+    res.sendFile(path.join(__dirname,"public","html","page3.html"));
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
